5dbde42ab67e75d069fe30e8edf3540b1ca1aeed
Ömer Alkan <omeralkn251907@gmail.com>
2025-07-28 14:32:00 +0300

    Panel eklendi

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,10 +15,14 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        User::create([
             'name' => 'Test User',
+            'surname' => 'Testoğlu', // ← EKLE BUNU
             'email' => 'test@example.com',
+            'role' => '1', // ← varsa role da zorunluysa ekle
+            'password' => bcrypt('123456789'),
         ]);
+
 
         $this->call([
             CityTownSeeder::class,
